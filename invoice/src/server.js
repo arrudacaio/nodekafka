c927ce19b94ee3f09ab4de3e43b398f901726611
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2022-01-02 18:07:18 -0300

    creates a consumer in invoice project

--- invoice/src/server.js
+++ invoice/src/server.js
@@ -3,5 +3,29 @@
 // acessível via REST
 import { Kafka } from 'kafkajs'
 
+const kafka = new Kafka({
+  brokers: ['localhost:9092'],
+  clientId: 'invoice'
+})
+
+const topic = 'issue-invoice'
+const consumer = kafka.consumer()
+
+const run = async () => {
+  await consumer.connect()
+  await consumer.subscribe({ topic })
+
+  await consumer.run({
+    eachMessage: async((payload) => {
+      console.log(`O payload completo: ${payload}`)
+      // const { topic, partition, message } = payload
+
+      // const prefix = `${topic}${partition}`
+    })
+  })
+}
 
 
+
+
+run().catch(console.error)
