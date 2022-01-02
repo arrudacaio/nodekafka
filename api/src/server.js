ce52201e9e6785489c2165f5c2cf5d1e048a3224
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2022-01-02 17:13:35 -0300

    adds kafka client in API

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,12 @@
 import express from 'express'
+// import Kafka from 'kafkajs'
 
 const app = express()
 
+// const kafka = new Kafka({
+//   clientId: 'my-app',
+//   brokers: ['kafka1:9092', 'kafka2:9092']
+// })
 
 app.post('/invoices', (req, res) => {
   // Call to a microservice
